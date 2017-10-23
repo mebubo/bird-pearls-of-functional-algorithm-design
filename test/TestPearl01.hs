ae525c10eb0ffeab838b9efaa75469e78fafc398
Sergei Dolgov <dolgovs@gmail.com>
2017-10-23 13:32:17 +0000

    TestPearl01: add a prop

--- test/TestPearl01.hs
+++ test/TestPearl01.hs
@@ -9,14 +9,23 @@ import Pearl01
 compareImpls :: Minfree -> Minfree -> [Natural] -> Bool
 compareImpls reference impl xs = impl xs == reference xs
 
+compareWithNaive :: Minfree -> [Natural] -> Bool
 compareWithNaive = compareImpls minfree
 
 runTests :: Testable prop => [prop] -> IO Bool
 runTests = ((all isSuccess) <$>) . sequence . (quickCheckResult <$>)
 
+minOfTwo :: Minfree -> [Natural] -> [Natural] -> Bool
+minOfTwo impl xs ys = min (impl xs) (impl ys) <= impl (xs ++ ys)
+
 main :: IO Bool
 main = do
-   runTests
+   r1 <- runTests
         [ compareWithNaive minfree'
         , compareWithNaive minfree''
         ]
+   r2 <- runTests
+        [ minOfTwo minfree'
+        , minOfTwo minfree''
+        ]
+   pure $ r1 && r2
